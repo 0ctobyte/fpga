d134822abe07be7f8acb1b702bba1b3a9417e193
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2017-07-10 03:20:46 -0400

    Update uart_test to use shared bus with seg7_controller and uart_controller as slaves. Implemented test master to drive the system

--- uart_test/uart_test.do.tcl
+++ uart_test/uart_test.do.tcl
@@ -1,7 +1,7 @@
 # ModelSim TCL Simulation Script
 
 set PROJECT uart_test
-set FILES {uart_test.sv ../hdl_lib/seg7_decoder/seg7_decoder.sv ../hdl_lib/synchronizer/synchronizer.sv ../hdl_lib/uart_rx/uart_rx.sv ../hdl_lib/uart_tx/uart_tx.sv}
+set FILES {uart_test.sv ../hdl_lib/biu_slave/biu_slave.sv ../hdl_lib/biu_master/biu_master.sv ../hdl_lib/chip_select/chip_select.sv ../hdl_lib/dp_ram/dp_ram.sv ../hdl_lib/seg7_controller/seg7_controller.sv ../hdl_lib/seg7_decoder/seg7_decoder.sv ../hdl_lib/sync_fifo/sync_fifo.sv ../hdl_lib/synchronizer/synchronizer.sv ../hdl_lib/uart_controller/uart_controller.sv ../hdl_lib/uart_rx/uart_rx.sv ../hdl_lib/uart_tx/uart_tx.sv}
 set TOP_LEVEL_ENTITY uart_test
 
 # Create a project if it doesn't exist
@@ -19,7 +19,7 @@ vsim $PROJECT.$TOP_LEVEL_ENTITY
 
 restart -force -nowave
 
-add wave /uart_test/*
+add wave /uart_test/* /uart_test_master_inst/* /uart_controller_inst/*
 
 force CLOCK_50 1 0ns, 0 10ns -repeat 20ns
 force {SW[17]} 0 0ns, 1 10ns
--- uart_test/uart_test.sv
+++ uart_test/uart_test.sv
@@ -1,8 +1,146 @@
+`define ADDR_WIDTH 32
+`define DATA_WIDTH 32
+`define FIFO_DEPTH 8
+`define UART_SLAVE_ADDR 32'hc0000000
+`define SEG7_SLAVE_ADDR 32'hc0001000
+
 `define CLK_FREQ  50000000
 `define BAUD_RATE 115200
 `define DATA_BITS 8
 `define STOP_BITS 1
 `define PARITY    0
+
+module uart_test_master #(
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32
+) (
+    input  wire                  clk,
+    input  wire                  n_rst,
+
+    // Bus interface
+    inout  wire [ADDR_WIDTH-1:0] bus_address,
+    inout  wire [DATA_WIDTH-1:0] bus_data,
+    inout  wire [1:0]            bus_control
+);
+
+    localparam IDLE = 6'b000001, RXFE = 6'b000010, RXRD = 6'b000100, S7WR = 6'b001000, TXFF = 6'b010000, TXWR = 6'b100000;
+
+    reg [5:0] state;
+    reg [`DATA_BITS-1:0] rx_data;
+    reg rxfe, txff;
+
+    // Interconnect between biu_test_master and biu_master
+    logic [ADDR_WIDTH-1:0] biu_master_address;
+    logic [DATA_WIDTH-1:0] biu_master_data_out;
+    logic                  biu_master_rnw;
+    logic                  biu_master_en;
+    wire  [DATA_WIDTH-1:0] biu_master_data_in;
+    wire                   biu_master_data_valid;
+    wire                   biu_master_busy;
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (~n_rst) begin
+            rx_data <= 'b0;
+        end else if (state == RXRD && biu_master_data_valid) begin
+            rx_data <= biu_master_data_in[`DATA_BITS-1:0];
+        end
+    end
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (~n_rst) begin
+            rxfe <= 'b1;
+        end else if (state == RXFE && biu_master_data_valid) begin
+            rxfe <= biu_master_data_in[0];
+        end else begin
+            rxfe <= 'b1;
+        end
+    end
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (~n_rst) begin
+            txff <= 'b1;
+        end else if (state == TXFF && biu_master_data_valid) begin
+            txff <= biu_master_data_in[1];
+        end else begin
+            txff <= 'b1;
+        end
+    end
+
+    always_ff @(posedge clk, negedge n_rst) begin
+        if (~n_rst) begin
+            state <= IDLE;
+        end else begin
+            case (state)
+                IDLE: state <= RXFE;
+                RXFE: state <= (~rxfe) ? RXRD : RXFE;
+                RXRD: state <= (biu_master_data_valid) ? S7WR : RXRD;
+                S7WR: state <= TXFF;
+                TXFF: state <= (~txff) ? TXWR : TXFF;
+                TXWR: state <= IDLE;
+            endcase
+        end 
+    end
+
+    always_comb begin
+        case (state)
+            IDLE: begin
+                biu_master_address  <= 'b0;
+                biu_master_data_out <= 'b0;
+                biu_master_rnw      <= 'b0;
+                biu_master_en       <= 'b0;
+            end
+            RXFE: begin
+                biu_master_address  <= `UART_SLAVE_ADDR + 8;
+                biu_master_data_out <= 'b0;
+                biu_master_rnw      <= 'b1;
+                biu_master_en       <= rxfe && ~biu_master_busy;
+            end
+            RXRD: begin
+                biu_master_address  <= `UART_SLAVE_ADDR;
+                biu_master_data_out <= 'b0;
+                biu_master_rnw      <= 'b1;
+                biu_master_en       <= ~biu_master_busy;
+            end
+            S7WR: begin
+                biu_master_address  <= `SEG7_SLAVE_ADDR;
+                biu_master_data_out <= rx_data;
+                biu_master_rnw      <= 'b0;
+                biu_master_en       <= ~biu_master_busy;
+            end
+            TXFF: begin
+                biu_master_address  <= `UART_SLAVE_ADDR + 8;
+                biu_master_data_out <= 'b0;
+                biu_master_rnw      <= 'b1;
+                biu_master_en       <= txff && ~biu_master_busy;
+            end
+            TXWR: begin
+                biu_master_address  <= `UART_SLAVE_ADDR;
+                biu_master_data_out <= rx_data;
+                biu_master_rnw      <= 'b0;
+                biu_master_en       <= ~biu_master_busy;
+            end
+        endcase
+    end
+
+    biu_master #(
+        .ADDR_WIDTH(ADDR_WIDTH),
+        .DATA_WIDTH(DATA_WIDTH)
+    ) biu_master0 (
+        .clk(clk),
+        .n_rst(n_rst),
+        .bus_address(bus_address),
+        .bus_data(bus_data),
+        .bus_control(bus_control),
+        .i_address(biu_master_address),
+        .i_data_out(biu_master_data_out),
+        .i_rnw(biu_master_rnw),
+        .i_en(biu_master_en),
+        .o_data_in(biu_master_data_in),
+        .o_data_valid(biu_master_data_valid),
+        .o_busy(biu_master_busy)
+    );
+
+endmodule
 
 module uart_test (
     input  wire                    CLOCK_50,
@@ -10,69 +148,57 @@ module uart_test (
 
     input  wire                    UART_RXD,
     output wire                    UART_TXD,
-    output wire [6:0]              HEX0,
-    output wire [6:0]              HEX1
+    output wire [6:0]              HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7
 );
 
-    reg [`DATA_BITS-1:0]  recv_data;
+    // Shared bus
+    tri [`ADDR_WIDTH-1:0] bus_address;
+    tri [`DATA_WIDTH-1:0] bus_data;
+    tri [1:0]             bus_control;
 
-    wire [`DATA_BITS-1:0] uart_rx_data;
-    wire                  uart_rx_data_valid;
-
-    wire [`DATA_BITS-1:0] uart_tx_data;
-    wire                  uart_tx_data_valid;
-    wire                  uart_tx_busy;
-
-    assign uart_tx_data_valid = uart_rx_data_valid;
-    assign uart_tx_data       = uart_rx_data;
-
-    always_ff @(posedge CLOCK_50, negedge SW[17]) begin
-        if (~SW[17]) begin
-            recv_data <= 'b0;
-        end else if (uart_rx_data_valid) begin
-            recv_data <= uart_rx_data;
-        end
-    end
-
-    uart_rx #(
-        .CLK_FREQ(`CLK_FREQ),
-        .BAUD_RATE(`BAUD_RATE),
-        .DATA_BITS(`DATA_BITS),
-        .STOP_BITS(`STOP_BITS),
-        .PARITY(`PARITY)
-    ) uart_rx_inst (
+    uart_test_master #(
+        .ADDR_WIDTH(`ADDR_WIDTH),
+        .DATA_WIDTH(`DATA_WIDTH)
+    ) uart_test_master_inst (
         .clk(CLOCK_50),
         .n_rst(SW[17]),
-        .i_rx(UART_RXD),
-        .o_data_valid(uart_rx_data_valid),
-        .o_data(uart_rx_data)
+        .bus_address(bus_address),
+        .bus_data(bus_data),
+        .bus_control(bus_control)
     );
 
-    uart_tx #(
+    uart_controller #(
+        .ADDR_WIDTH(`ADDR_WIDTH),
+        .DATA_WIDTH(`DATA_WIDTH),
+        .BASE_ADDR(`UART_SLAVE_ADDR),
+        .FIFO_DEPTH(`FIFO_DEPTH),
         .CLK_FREQ(`CLK_FREQ),
         .BAUD_RATE(`BAUD_RATE),
         .DATA_BITS(`DATA_BITS),
         .STOP_BITS(`STOP_BITS),
         .PARITY(`PARITY)
-    ) uart_tx_inst (
+    ) uart_controller_inst (
         .clk(CLOCK_50),
         .n_rst(SW[17]),
-        .i_data_valid(uart_tx_data_valid),
-        .i_data(uart_tx_data),
-        .o_busy(uart_tx_busy),
+        .bus_address(bus_address),
+        .bus_data(bus_data),
+        .bus_control(bus_control),
+        .i_rx(UART_RXD),
         .o_tx(UART_TXD)
     );
 
-    seg7_decoder hex0 (
+    seg7_controller #(
+        .ADDR_WIDTH(`ADDR_WIDTH),
+        .DATA_WIDTH(`DATA_WIDTH),
+        .BASE_ADDR(`SEG7_SLAVE_ADDR),
+        .NUM_7SEGMENTS(8)
+    ) seg7_controller_inst (
+        .clk(CLOCK_50),
         .n_rst(SW[17]),
-        .i_hex(recv_data[3:0]),
-        .o_hex(HEX0)
-    );
-
-    seg7_decoder hex1 (
-        .n_rst(SW[17]),
-        .i_hex(recv_data[7:4]),
-        .o_hex(HEX1)
+        .bus_address(bus_address),
+        .bus_data(bus_data),
+        .bus_control(bus_control),
+        .o_hex({HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7})
     );
 
 endmodule
